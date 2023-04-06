eb9b1d0e81cc0f27e6fdb507bbcdf86904e2bd48
Pingoe88 <roy.logtenberg@eb-design.nl>
2023-04-06 11:12:14 +0200

    Added Licenseplate check

--- Gatekeeper/Gatekeeper.go
+++ Gatekeeper/Gatekeeper.go
@@ -1,24 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"bufio"   // Bibliotheek voor buffered I/O, waardoor het efficiënter is om gegevens te lezen en te schrijven.
+	"fmt"     // Bibliotheek voor formattering van output en input.
+	"os"      // Bibliotheek voor communicatie met het besturingssysteem.
+	"strings" // Bibliotheek voor string manipulatie.
+	"time"    // Bibliotheek voor tijd manipulatie.
 )
 
-func main() {
-	currentTime := time.Now()
-	timeStr := currentTime.Format("15:04")
-	welkomStr := ("Welkom bij Fonteyn Vakantieparken")
+// Functie voor het controleren of een kenteken in de lijst met toegestane kentekens zit.
+func checkName(kenteken string) bool {
+	allowedkenteken := []string{"82nnk5", "nk76b5", "nnk825", "825nnk"}
 
-	if timeStr >= "07:00" && timeStr <= "12:00" {
-		fmt.Println("Goedemorgen!:", welkomStr)
-	} else if timeStr > "12:00" && timeStr <= "18:00" {
-		fmt.Println("Goedemiddag:", welkomStr)
-	} else if timeStr > "18:00" && timeStr <= "23:00" {
-		fmt.Println("Goedenavond:", welkomStr)
-	} else {
-		fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten. The current time is:")
+	kenteken = strings.ToLower(kenteken) // Zet het ingevoerde kenteken in kleine letters.
+	for _, allowed := range allowedkenteken {
+		if kenteken == allowed {
+			return true // Kenteken is toegestaan.
+		}
 	}
+
+	return false // Kenteken is niet toegestaan.
 }
 
-//V2
+func main() {
+	welkomStr := "Welkom bij Fonteyn Vakantieparken"
+	currentTime := time.Now().Format("15:04") // Vind de huidige tijd en zet het in uur:minuut formaat.
+
+	fmt.Print("Voer je kenteken in: ")
+	reader := bufio.NewReader(os.Stdin)    // Maak een nieuwe reader aan voor input van de gebruiker.
+	kenteken, _ := reader.ReadString('\n') // Lees de ingevoerde string tot aan de volgende nieuwe regel.
+	kenteken = strings.TrimSpace(kenteken) // Verwijder eventuele extra spaties voor of na het kenteken.
+
+	if checkName(kenteken) {
+		if currentTime >= "07:00" && currentTime <= "12:00" {
+			fmt.Println("Goedemorgen!:", welkomStr)
+		} else if currentTime > "12:00" && currentTime <= "18:00" {
+			fmt.Println("Goedemiddag:", welkomStr)
+		} else if currentTime > "18:00" && currentTime <= "23:00" {
+			fmt.Println("Goedenavond:", welkomStr)
+		} else {
+			fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten.")
+		}
+	} else {
+		fmt.Println("U heeft helaas geen toegang tot het parkeerterrein.")
+	}
+}
